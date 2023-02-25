92333d1e04e4a78a477a26125a6bee9beddf3fa9
ernest-tt <122813064+ernest-tt@users.noreply.github.com>
2023-02-25 19:30:22 +0000

    mobile version static page

---
+++ main.js
@@ -0,0 +1,4 @@
+//get the progress bar element with #bar
+//make a fetch request to get the current amount raised
+// elem.style.width = current/total expressed in percentage + %
+// animate the value from zero to current
